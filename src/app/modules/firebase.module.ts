67926edf13487806af563ce41ac8722fc1e4632a
Guilherme Costamilam <guilherme@Inspiron.Costamilam>
2019-03-14 20:04:59 -0300

    Storage module added

--- src/app/modules/firebase.module.ts
+++ src/app/modules/firebase.module.ts
@@ -3,6 +3,7 @@ import { CommonModule } from '@angular/common';
 
 import { AngularFireModule } from '@angular/fire';
 import { AngularFireDatabaseModule } from '@angular/fire/database';
+import { AngularFireStorageModule } from '@angular/fire/storage';
 import { AngularFireAuthModule } from '@angular/fire/auth';
 
 import { environment } from 'src/environments/environment';
@@ -13,6 +14,7 @@ import { environment } from 'src/environments/environment';
         CommonModule,
         AngularFireModule.initializeApp(environment.firebase),
         AngularFireDatabaseModule,
+        AngularFireStorageModule,
         AngularFireAuthModule
     ]
 })
