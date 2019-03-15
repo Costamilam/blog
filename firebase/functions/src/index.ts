a1124367cbbb0aa605d29617821ee3085b1def3d
Guilherme Costamilam <guilherme@Inspiron.Costamilam>
2019-03-15 03:55:13 -0300

    added functions to set date on create and to delete on delete related

--- firebase/functions/src/index.ts
+++ firebase/functions/src/index.ts
@@ -6,13 +6,47 @@ admin.initializeApp();
 // Start writing Firebase Functions
 // https://firebase.google.com/docs/functions/typescript
 
-export const setDate = functions.database.ref('/posts/{id}').onCreate((snapshot, context) => {
+export const setPostDate = functions.database.ref('/posts/{id}').onCreate((snapshot, context) => {
     const data = snapshot.val();
 
     data.date = new Date().toISOString();
 
     snapshot.ref.set(data).catch(error => {
         console.error('Failed to add date when creating post: ', error)
+    });
+});
+
+export const removePostComments = functions.database.ref('/posts/{id}').onDelete((snapshot, context) => {
+    admin.database().ref('/comments').orderByChild('reply').equalTo(context.params.id).once('value').then(value => {
+        value.forEach(item => {
+            item.ref.remove().catch(error => {
+                console.error('failed to remove comment when deleting post: ', error)
+            });
+        })
+    }).catch(error => {
+        console.error('Failed to get comments while deleting post: ', error)
+    });
+});
+
+export const setCommentDate = functions.database.ref('/comments/{id}').onCreate((snapshot, context) => {
+    const data = snapshot.val();
+
+    data.date = new Date().toISOString();
+
+    snapshot.ref.set(data).catch(error => {
+        console.error('Failed to add date when creating comment: ', error)
+    });
+});
+
+export const removeCommentComments = functions.database.ref('/comments/{id}').onDelete((snapshot, context) => {
+    admin.database().ref('/comments').orderByChild('reply').equalTo(context.params.id).once('value').then(value => {
+        value.forEach(item => {
+            item.ref.remove().catch(error => {
+                console.error('failed to remove comment when deleting comment: ', error)
+            });
+        })
+    }).catch(error => {
+        console.error('Failed to get comments while deleting comment: ', error)
     });
 });
 
@@ -36,6 +70,16 @@ export const removeUserFromDatabase = functions.auth.user().onDelete((user, cont
         })
     }).catch(error => {
         console.error('Failed to get user posts while deleting user: ', error)
+    });
+
+    admin.database().ref('/comments').orderByChild('uid').equalTo(user.uid).once('value').then(value => {
+        value.forEach(item => {
+            item.ref.remove().catch(error => {
+                console.error('failed to remove comment when deleting user: ', error)
+            });
+        })
+    }).catch(error => {
+        console.error('Failed to get user comments while deleting user: ', error)
     });
 
     admin.database().ref(`/users/${user.uid}`).remove().catch(error => {
