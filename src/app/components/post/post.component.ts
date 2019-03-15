a827c1c4ec2b6f24dae33ee237061b5ce839affb
Guilherme Costamilam <guilherme@Inspiron.Costamilam>
2019-03-15 03:51:02 -0300

    Comments added

--- src/app/components/post/post.component.ts
+++ src/app/components/post/post.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 
+import { MatSnackBar } from '@angular/material';
+
 import MarkdownIt from 'markdown-it';
+
 import { PostService } from 'src/app/services/post.service';
 import { UserService } from 'src/app/services/user.service';
-import { MatSnackBar } from '@angular/material';
+import { CommentService } from 'src/app/services/comment.service';
 
 @Component({
     selector: 'app-post',
@@ -15,25 +18,30 @@ export class PostComponent {
 
     markdown = MarkdownIt();
 
+    id;
+
     post;
 
-    user;
+    users = {};
+
+    comments;
 
     constructor(
         private router: Router,
         private route: ActivatedRoute,
         private postService: PostService,
         private userService: UserService,
+        private commentsService: CommentService,
         private snackBar: MatSnackBar
     ) {
-        const id = this.route.snapshot.params.id;
+        this.id = this.route.snapshot.params.id;
 
-        this.postService.getPost(id).subscribe((post: any) => {
+        this.postService.getPost(this.id).subscribe((post: any) => {
             if (!post) {
-                this.snackBar.open('Post não encontrado!', null, {
+                this.snackBar.open('Postagem não encontrado!', null, {
                     duration: 2000,
                 });
-    
+
                 this.router.navigate(['/posts']);
             }
 
@@ -41,7 +49,21 @@ export class PostComponent {
 
             this.post = post;
 
-            this.userService.getUser(post.uid).subscribe(user => this.user = user);
+            this.userService.getUser(post.uid).subscribe(user => {
+                this.users[post.uid] = user;
+
+                this.commentsService.getComments(this.id).subscribe((comments: any[]) => {
+                    this.comments = comments.map(comment => {
+                        comment = { id: comment.key, ...comment.payload.val() };
+
+                        if (!this.users[comment.uid]) {
+                            this.userService.getUser(comment.uid).subscribe(user => this.users[comment.uid] = user);
+                        }
+
+                        return comment;
+                    });
+                });
+            });
         });
     }
 
